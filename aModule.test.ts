c20ee43154c2697274a212235fa20c46e0c3dabb
Nathaniel Ho <nathaniel.ho@sisense.com>
2023-08-15 10:37:33 -0700

    replace vitest with jest to test spyOn

--- aModule.test.ts
+++ aModule.test.ts
@@ -1,8 +1,8 @@
-import { vi, expect, test } from "vitest";
+// import { vi, expect, test } from "vitest";
 import * as aModule from "./aModule";
 
 test('expect "sideEffect" to be called at least once', async () => {
-  const sideEffectSpy = vi.spyOn(aModule, "sideEffect").mockReturnValue("b");
+  const sideEffectSpy = jest.spyOn(aModule, "sideEffect").mockReturnValue("b");
 
   const res = aModule.a(); // This function calls sideEffect internally.
   expect(res).toBe("b"); // This fails - it returns 'a' so the spyOn is not workng
